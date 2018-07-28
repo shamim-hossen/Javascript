4afed2b5b5a1ade657d6a2652c34cde478df02b6
Shamim Hossen <36667631+shamim-hossen@users.noreply.github.com>
2018-07-28 23:44:29 +0600

    Create problem.js

---
+++ problem.js
@@ -0,0 +1,21 @@
+//s1 =2 4 6 8 10 12 14 ...
+
+var series = "";
+var name = "mim ";
+var n;
+for (var i=1;i<10;i++){
+    n = i*2;
+    name = name + n +" ";
+}
+console.log(name);
+
+
+//s2 = 1 4 7 10 13 16...
+
+series ="1 ";
+n = 1;
+for(var i=1;i<10;i++){
+    n = n+3;
+    series = series + n + " ";
+}
+console.log("series 2 =",series);
